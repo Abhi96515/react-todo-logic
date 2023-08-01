d2162e6eef5fc94e7e3ee9360314fec5a8a7cb09
Abhi96515 <103572307+Abhi96515@users.noreply.github.com>
2023-08-01 14:57:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+  import React , {useState} from 'react'
+  import './App.css'
+  import Todo from './Todo'
+
+
+  const App = () => {
+    return (
+      <div>
+        <Todo/>
+      </div>
+    )
+  }
+
+  export default App
